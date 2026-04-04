bfaa91c5c21152b498b80602bcd89cceb3af2e44
halucineight <halucineight@users.noreply.github.com>
2026-04-04 09:27:30 +0000

    Add distro-based binary selection for lsp

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -82,6 +82,64 @@ return {
       "mason-org/mason.nvim",
       "mason-org/mason-lspconfig.nvim",
     },
+    config = function()
+      local function is_executable(path)
+        return path ~= nil and vim.fn.executable(path) == 1
+      end
+
+      local function is_mason_bin(path)
+        return path ~= nil and path:match("/%.local/share/nvim/mason/bin/")
+      end
+
+      local function system_binary(bin)
+        local path = "/run/current-system/sw/bin/" .. bin
+
+        if is_executable(path) then
+          return path
+        end
+      end
+
+      local function first_non_mason_path_binary(bin)
+        for dir in string.gmatch(vim.env.PATH or "", "([^:]+)") do
+          local path = dir .. "/" .. bin
+
+          if not is_mason_bin(path) and is_executable(path) then
+            return path
+          end
+        end
+      end
+
+      local function preferred_executable(bin)
+        return system_binary(bin)
+          or first_non_mason_path_binary(bin)
+          or (vim.fn.exepath(bin) ~= "" and vim.fn.exepath(bin))
+      end
+
+      local lua_ls = preferred_executable("lua-language-server")
+      local stylua = preferred_executable("stylua")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      require("mason-lspconfig").setup({
+        ensure_installed = lua_ls and {} or { "lua_ls" },
+      })
+
+      vim.lsp.config("lua_ls", lua_ls and {
+        cmd = { lua_ls },
+        capabilities = capabilities,
+      } or {
+        capabilities = capabilities,
+      })
+
+      vim.lsp.enable("lua_ls")
+
+      if stylua then
+        vim.lsp.config("stylua", {
+          cmd = { stylua, "--lsp" },
+        })
+        vim.lsp.enable("stylua")
+      end
+    end,
+  },
   {
     "mason-org/mason.nvim",
     config = function()
