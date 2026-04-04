b80c47830c5897c2e27d0f2072c5ad8805b0b87f
halucineight <halucineight@users.noreply.github.com>
2026-04-04 09:15:25 +0000

    Add autocomplete

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -22,6 +22,28 @@ return {
     end,
   },
   {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+    },
+    config = function()
+      local cmp = require("cmp")
+
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+        }, {
+          { name = "buffer" },
+        }),
+      })
+    end,
+  },
+  {
     "nvim-treesitter/nvim-treesitter",
     branch = "master",
     lazy = false,
