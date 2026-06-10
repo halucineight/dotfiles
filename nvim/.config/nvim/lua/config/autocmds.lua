11ffad01b03055f7d75afaf16a5c045c2081595a
halucineight <halucineight@users.noreply.github.com>
2026-06-10 10:51:34 +0000

    Remove mason

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -3,10 +3,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(args)
     local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
 
-    if
-      not client:supports_method("textDocument/willSaveWaitUntil")
-      and client:supports_method("textDocument/formatting")
-    then
+    if client:supports_method("textDocument/formatting") then
       vim.api.nvim_create_autocmd("BufWritePre", {
         group = vim.api.nvim_create_augroup("my.lsp", { clear = false }),
         buffer = args.buf,
--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,271 +1,248 @@
 return {
-  {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("gruvbox")
-    end,
-  },
-  {
-    "NeogitOrg/neogit",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    config = function()
-      require("neogit").setup({})
-    end,
-  },
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup({})
-    end,
-  },
-  {
-    "romgrk/barbar.nvim",
-    dependencies = {
-      "lewis6991/gitsigns.nvim",
-      "nvim-tree/nvim-web-devicons",
-    },
-    init = function()
-      vim.g.barbar_auto_setup = false
-    end,
-    opts = {},
-    version = "^1.0.0",
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    config = function()
-      require("telescope").setup({
-        defaults = {
-          initial_mode = "normal",
-        },
-        pickers = {
-          find_files = {
-            hidden = true,
-            find_command = {
-              "rg",
-              "--files",
-              "--hidden",
-              "--glob",
-              "!**/.git/*",
-            },
-          },
-          live_grep = {
-            additional_args = function()
-              return { "--hidden", "--glob", "!**/.git/*" }
-            end,
-          },
-        },
-      })
-    end,
-  },
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-      require("lualine").setup({
-        options = {
-          theme = "gruvbox",
-          component_separators = { left = "|", right = "|" },
-          section_separators = { left = "", right = "" },
-        },
-        sections = {
-          lualine_a = { "mode" },
-          lualine_b = { "branch", "diff", "diagnostics" },
-          lualine_c = { "filename" },
-          lualine_x = { "encoding", "fileformat", "filetype" },
-          lualine_y = { "progress" },
-          lualine_z = { "location" },
-        },
-      })
-    end,
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "windwp/nvim-autopairs",
-    },
-    config = function()
-      local cmp = require("cmp")
+	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("gruvbox")
+		end,
+	},
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			require("neogit").setup({})
+		end,
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup({})
+		end,
+	},
+	{
+		"romgrk/barbar.nvim",
+		dependencies = {
+			"lewis6991/gitsigns.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+		init = function()
+			vim.g.barbar_auto_setup = false
+		end,
+		opts = {},
+		version = "^1.0.0",
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			require("telescope").setup({
+				defaults = {
+					initial_mode = "normal",
+				},
+				pickers = {
+					find_files = {
+						hidden = true,
+						find_command = {
+							"rg",
+							"--files",
+							"--hidden",
+							"--glob",
+							"!**/.git/*",
+						},
+					},
+					live_grep = {
+						additional_args = function()
+							return { "--hidden", "--glob", "!**/.git/*" }
+						end,
+					},
+				},
+			})
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("lualine").setup({
+				options = {
+					theme = "gruvbox",
+					component_separators = { left = "|", right = "|" },
+					section_separators = { left = "", right = "" },
+				},
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "branch", "diff", "diagnostics" },
+					lualine_c = { "filename" },
+					lualine_x = { "encoding", "fileformat", "filetype" },
+					lualine_y = { "progress" },
+					lualine_z = { "location" },
+				},
+			})
+		end,
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"windwp/nvim-autopairs",
+		},
+		config = function()
+			local cmp = require("cmp")
 
-      cmp.setup({
-        mapping = cmp.mapping.preset.insert({
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-        }, {
-          { name = "buffer" },
-        }),
-      })
+			cmp.setup({
+				mapping = cmp.mapping.preset.insert({
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+				}, {
+					{ name = "buffer" },
+				}),
+			})
 
-      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-    end,
-  },
-  {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    config = function()
-      require("nvim-autopairs").setup({})
-    end,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    branch = "main",
-    lazy = false,
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter").setup({
-        install_dir = vim.fn.stdpath("data") .. "/site",
-      })
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+		end,
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = function()
+			require("nvim-autopairs").setup({})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		lazy = false,
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter").setup({
+				install_dir = vim.fn.stdpath("data") .. "/site",
+			})
 
-      require("nvim-treesitter").install({ "lua", "vim", "vimdoc", "nix", "elixir", "eex", "heex" })
+			require("nvim-treesitter").install({ "lua", "vim", "vimdoc", "nix", "elixir", "eex", "heex" })
 
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = { "lua", "vim", "help", "nix", "elixir", "eelixir", "eex", "heex", "surface" },
-        callback = function()
-          vim.treesitter.start()
-          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-        end,
-      })
-    end,
-  },
-  {
-    "stevearc/oil.nvim",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-      require("oil").setup({
-        default_file_explorer = true,
-      })
-    end,
-  },
-  {
-    "akinsho/toggleterm.nvim",
-    version = "*",
-    config = function()
-      require("toggleterm").setup({
-        direction = "horizontal",
-        float_opts = {
-          border = "curved",
-        },
-      })
-    end,
-  },
-  {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-      require("nvim-tree").setup({})
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "mason-org/mason.nvim",
-      "mason-org/mason-lspconfig.nvim",
-    },
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = { "lua", "vim", "help", "nix", "elixir", "eelixir", "eex", "heex", "surface" },
+				callback = function()
+					vim.treesitter.start()
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end,
+			})
+		end,
+	},
+	{
+		"stevearc/oil.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("oil").setup({
+				default_file_explorer = true,
+			})
+		end,
+	},
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		config = function()
+			require("toggleterm").setup({
+				direction = "horizontal",
+				float_opts = {
+					border = "curved",
+				},
+			})
+		end,
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("nvim-tree").setup({})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      require("mason-lspconfig").setup({})
+			vim.lsp.config("lua_ls", {
+				capabilities = capabilities,
+			})
 
-      vim.lsp.config("lua_ls", {
-        capabilities = capabilities,
-      })
+			vim.lsp.enable("lua_ls")
 
-      vim.lsp.enable("lua_ls")
+			vim.lsp.config("qmlls", {
+				filetypes = { "qml", "qmljs" },
+				capabilities = capabilities,
+				settings = {
+					qml = {
+						formatCommand = { "qmlformat" },
+					},
+				},
+			})
+			vim.lsp.enable("qmlls")
 
-      vim.lsp.config("qmlls", {
-        filetypes = { "qml", "qmljs" },
-        capabilities = capabilities,
-        settings = {
-          qml = {
-            formatCommand = { "qmlformat" },
-          },
-        },
-      })
-      vim.lsp.enable("qmlls")
+			vim.lsp.config("stylua", {
+				cmd = { "stylua", "--lsp" },
+			})
+			vim.lsp.enable("stylua")
 
-      vim.lsp.config("stylua", {
-        cmd = { "stylua", "--lsp" },
-      })
-      vim.lsp.enable("stylua")
+			vim.lsp.config("elixirls", {
+				cmd = { "elixir-ls" },
+				capabilities = capabilities,
+				settings = {
+					elixirLS = {
+						dialyzerEnabled = true,
+						fetchDeps = false,
+						enableTestLenses = false,
+						suggestSpecs = true,
+					},
+				},
+			})
+			vim.lsp.enable("elixirls")
 
-      vim.lsp.config("elixirls", {
-        cmd = { "elixir-ls" },
-        capabilities = capabilities,
-        settings = {
-          elixirLS = {
-            dialyzerEnabled = true,
-            fetchDeps = false,
-            enableTestLenses = false,
-            suggestSpecs = true,
-          },
-        },
-      })
-      vim.lsp.enable("elixirls")
+			vim.lsp.config("nixd", {
+				capabilities = capabilities,
+				settings = {
+					nixd = {
+						formatting = {
+							command = { "nixfmt" },
+						},
+					},
+				},
+			})
+			vim.lsp.enable("nixd")
 
-      vim.lsp.config("nixd", {
-        capabilities = capabilities,
-        settings = {
-          nixd = {
-            formatting = {
-              command = { "nixfmt" },
-            },
-          },
-        },
-      })
-      vim.lsp.enable("nixd")
-      
-      vim.lsp.config("rust_analyzer", {
-        capabilities = capabilities,
-        settings = {
-          ["rust-analyzer"] = {
-            rustfmt = {
-              overrideCommand = { "rustfmt" },
-            },
-          },
-        },
-      })
-      vim.lsp.enable("rust_analyzer")
-
-    end,
-  },
-  {
-    "mason-org/mason.nvim",
-    config = function()
-      require("mason").setup({})
-    end,
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-  },
+			vim.lsp.config("rust_analyzer", {
+				capabilities = capabilities,
+			})
+			vim.lsp.enable("rust_analyzer")
+		end,
+	},
 }
