a24dc5371832bba7e30670b7b678171875cc7218
halucineight <halucineight@users.noreply.github.com>
2026-07-30 13:44:26 +0000

    Add window rules and keybinds

---
+++ hyprland/.config/hypr/windowrules.lua
@@ -0,0 +1,12 @@
+hl.window_rule({
+    match = { class = "firefox" },
+    opacity = "1.0 override 0.5 override 1 override",
+})
+hl.window_rule({
+    match = { class = "gimp" },
+    opacity = "1.0 override 1 override 1 override",
+})
+hl.window_rule({
+    match = { class = "org.kde.kdenlive" },
+    opacity = "1.0 override 0.5 override 1 override",
+})
